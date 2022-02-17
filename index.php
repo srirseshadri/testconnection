8e55509b233a56d7c0742df367d1c58357e1ef01
srirseshadri <35701265+srirseshadri@users.noreply.github.com>
2022-02-17 15:55:23 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+<title>Hello World!</title>
+</head>
+
+<body>
+
+<?php echo "Hello World! Welcome to our Test Page"; ?>
+<br>
+  <h2> Connection Works <h2>
+
+
+
+</body>
+</html>
